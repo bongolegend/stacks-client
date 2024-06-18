40429e92f88fed27d17eed20e0c401baacc80d63
bongolegend <nicomailbox@protonmail.com>
2024-06-18 17:44:16 -0400

    update compiler

--- project_compiler.py
+++ project_compiler.py
@@ -1,18 +1,28 @@
 import os
 
-def create_master_file(directory, master_file):
-    if os.path.exists(master_file):
-        os.remove(master_file)
-    with open(master_file, 'w') as master:
-        for file in os.listdir(directory):
-            file_path = os.path.join(directory, file)
+def append_files_to_master(directory, master, subdir=''):
+    path = os.path.join(directory, subdir)
+    if os.path.exists(path) and os.path.isdir(path):
+        for file in os.listdir(path):
+            file_path = os.path.join(path, file)
             if file_path.endswith(('.ts', '.tsx')) and os.path.isfile(file_path):
+                relative_path = os.path.relpath(file_path, directory)
                 with open(file_path, 'r') as f:
-                    master.write(f"// Filename: {file}\n")
-                    print(f"Starting Filename: {file}")
+                    master.write(f"// Filename: {relative_path}\n")
+                    print(f"Starting Filename: {relative_path}")
                     master.write(f.read())
                     master.write("\n\n")  # Add new lines between files for readability
-                    print(f"Finished Filename: {file}")
+
+def create_master_file(directory, master_file):
+    specific_folders = ['.', 'components', 'screens', 'navigation', 'utils', 'services', 'contexts']
+
+    if os.path.exists(master_file):
+        os.remove(master_file)
+
+    with open(master_file, 'w') as master:
+        # Search in the root directory and specific subdirectories
+        for folder in specific_folders:
+            append_files_to_master(directory, master, folder)
 
 directory = '/Users/nico/code/stacks-client'  # Replace with the path to your directory
 master_file = f'{directory}/master_file.txt'  # Replace with the path to your master file
